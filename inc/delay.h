1b8f3fe8fe1b1852cdad3f046c4231b59acc2b6e
Aidan Orr <aidanorr@comcast.net>
2022-11-12 14:06:12 -0500

    Added delayMilliseconds function Added .clang-format

--- inc/delay.h
+++ inc/delay.h
@@ -1,30 +1,40 @@
 /**
  * @file delay.h
  * @author Purdue Solar Racing (Aidan Orr)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2022-11-12
- * 
+ *
  * @copyright Copyright (c) 2022
- * 
+ *
  */
 
 #ifndef __DELAY_H
 #define __DELAY_H
 
+#include "stm32/auto_include.h"
 #include <stdbool.h>
 #include <stdint.h>
-#include "stm32/auto_include.h"
 
 namespace PSR
 {
-	/**
-	 * @brief Wait for a specific amount of microseconds before continuing.
-	 * 
-	 * @param timer1MHz A reference to a 1MHz timer.
-	 * @param microseconds The number of microseconds to delay.
-	 */
-	void delayMicroseconds(TIM_HandleTypeDef* timer1MHz, uint32_t microseconds);
-}
+
+/**
+ * @brief Wait for a specific number of microseconds before continuing.
+ *
+ * @param timer1MHz A reference to a 1MHz timer.
+ * @param microseconds The number of microseconds to delay.
+ */
+void delayMicroseconds(TIM_HandleTypeDef* timer1MHz, uint32_t microseconds);
+
+/**
+ * @brief Wait for a specific number of milleseconds before continuing.
+ *
+ * @param timer1MHz A reference to a 1MHz timer.
+ * @param millesconds Thre number of milleseconds to delay.
+ */
+void delayMilliseconds(TIM_HandleTypeDef* timer1MHz, uint32_t milliseconds);
+
+} // namespace PSR
 
 #endif __DELAY_H // End of include guard for delay.h
--- src/delay.cpp
+++ src/delay.cpp
@@ -2,9 +2,18 @@
 
 namespace PSR
 {
-	void delayMicroseconds(TIM_HandleTypeDef* timer1MHz, uint16_t microseconds)
+
+void delayMicroseconds(TIM_HandleTypeDef* timer1MHz, uint32_t microseconds)
+{
+	__HAL_TIM_SET_COUNTER(timer1MHz, 0); // set the counter value a 0
+	while (__HAL_TIM_GET_COUNTER(timer1MHz) < us)
 	{
-		__HAL_TIM_SET_COUNTER(timer1MHz,0); 				// set the counter value a 0
-		while (__HAL_TIM_GET_COUNTER(timer1MHz) < us);		// wait for the counter to reach the us input in the parameter
-	}
+	} // wait for the counter to reach the us input in the parameter
+}
+
+void delayMilliseconds(TIM_HandleTypeDef* timer1MHz, uint32_t milliseconds)
+{
+	delayMicroseconds(timer1MHz, 1000 * milliseconds);
+}
+
 } // namespace PSR
